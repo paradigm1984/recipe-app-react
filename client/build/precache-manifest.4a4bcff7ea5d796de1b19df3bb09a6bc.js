668f84b6b5f21d218089289d378b7fd2a6359b16
Daniel Blanco <dan@danblanco.com>
2020-06-26 16:18:45 -0400

    setting up production build

---
+++ client/build/precache-manifest.4a4bcff7ea5d796de1b19df3bb09a6bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4dbc1e023917fe959ffac616ca7256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72885386b9040f361ca1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e0d01a217acd504d61b",
+    "url": "/static/css/main.2e67de10.chunk.css"
+  },
+  {
+    "revision": "72885386b9040f361ca1",
+    "url": "/static/js/2.63413cc7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.63413cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0d01a217acd504d61b",
+    "url": "/static/js/main.705fa7bc.chunk.js"
+  },
+  {
+    "revision": "551c8aeb33574eeb1c6b",
+    "url": "/static/js/runtime-main.95d0240a.js"
+  }
+]);
