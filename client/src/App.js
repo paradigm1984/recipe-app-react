af3b554b79b9b74effa95f6788386782eb5cc89b
Daniel Blanco <dan@danblanco.com>
2020-06-27 12:08:18 -0400

    basic filter system based on health factors added

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React,{useEffect, useState} from 'react';
 import Recipie from './Recipe';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.scss';
-import { InputGroup, FormControl, Button, Row } from 'react-bootstrap';
+import { InputGroup, Form, Button, Row } from 'react-bootstrap';
 
 const App = ()=> {
 
@@ -11,12 +11,13 @@ const App = ()=> {
   const [recipies, setRecipies] = useState([]);  
   const [searchString, setSearchString] = useState("");
   const [queryString, setQueryString] = useState("");
+  const [filter, setFilter] = useState("");
   const [appDimensions, setAppDimensions] = React.useState({width: window.innerWidth, height: window.innerHeight});
 
   // by adding an empty array to the useEffect function as a param, itll only run when the app has mounted. you could add the state in which you want this function to run within the array.
-  useEffect(()=> {
-    getRecipies();
-  }, [queryString]);
+  // useEffect(()=> {
+  //   getRecipies();
+  // }, [queryString]);
 
   useEffect(() => {
     window.addEventListener("resize", updateWidthAndHeight);
@@ -30,22 +31,39 @@ const App = ()=> {
   // async functions are a way to use promises. similar to .then().
   const getRecipies = async () => {
     // gives you an await option that will make JS wait until the data is recieved and then assigns it to a var. especially useful when getting a response from an API.
-    const response = await fetch(`https://api.edamam.com/search?q=${queryString}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    // console.log("data: ", data.hits);
+    let response;
+    let data;
+
+    switch (filter) {
+      case "":
+        response = await fetch(`https://api.edamam.com/search?q=${queryString}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        console.log("response no filter: ", response);
+        data = await response.json();
+        break;
+      default:
+        response = await fetch(`https://api.edamam.com/search?q=${queryString}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${filter}`);
+        console.log("response with filter: ", response);
+        data = await response.json();
+      }
+
+    console.log("data: ", data.hits);
     setRecipies(data.hits);
-    console.log(data.hits);
   }
 
   // seems to be an ES6 way of writing a function for events
   const updateSearch = e => {
-    setSearchString(e.target.value)
-    // console.log("search string:", searchString)
+    setSearchString(e.target.value);
+  }
+
+  const updateFilter = e => {
+    setFilter(e.target.value);
   }
 
   const submitSearch = e => {
     e.preventDefault();
+    console.log(`submitting search with ${queryString} as the query string`);
     setQueryString(searchString);
+    getRecipies(queryString);
     setSearchString("");
     updateWidthAndHeight();
   }
@@ -53,36 +71,54 @@ const App = ()=> {
   return (
     <div className="app" style={appDimensions}>
       <section className="submit-section">
-        <h2 className="app-header" >Recipe Search</h2>
-        <form  onSubmit={submitSearch}>
-        <InputGroup className="mb-3">
-          <FormControl
-            placeholder="Search a recipe"
-            aria-label="Recipe"
-            aria-describedby="basic-addon2"
-            className="search-bar"
-            type="text"
-            value={searchString}
-            onChange={updateSearch}
-          />
-      <InputGroup.Append>
-        <Button variant="outline-secondary" className="search-button">Submit</Button>
-      </InputGroup.Append>
-    </InputGroup>
-        </form>
+        <h2 className="app-header-1" >Recipe Search</h2>
+        <Form onSubmit={submitSearch}>
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Search a recipe"
+              aria-label="Recipe"
+              aria-describedby="basic-addon2"
+              className="search-bar"
+              type="text"
+              value={searchString}
+              onChange={updateSearch}
+            />
+            <InputGroup.Append>    
+              <Button variant="outline-secondary" type="submit" className="search-button">Submit</Button>
+            </InputGroup.Append>
+          </InputGroup>
+          <div className="filter-select-list">
+            <h3 className="app-header-2" >Filters</h3>
+            <Form.Control
+              as="select"
+              id="inlineFormCustomSelect"
+              className="custom-select"
+              onChange={updateFilter}
+            >
+              <option value="0">Choose...</option>
+              <option value="vegan">Vegan</option>
+              <option value="vegetarian">Vegetarian</option>
+              <option value="peanut-free">Peanut-Free</option>
+              <option value="tree-nut-free">Tree-Nut-Free</option>
+              <option value="peanut-free">Peanut-Free</option>
+            </Form.Control>
+          </div>
+        </Form>
    
       </section>
       <section className="response-section">
-        <Row>
+        <Row className="recipie-row">
           {/* mapping the array of objects which is being pulled in by the API.
         uses the Recipie component to lay it all out in HTML (JSX) */}
           {recipies.map(recipe => (
             <Recipie
               key={recipe.recipe.uri}
               title={recipe.recipe.label}
-              calories={recipe.recipe.calories}
+              calories={recipe.recipe.calories.toFixed(0)}
               image={recipe.recipe.image}
               ingredients={recipe.recipe.ingredients}
+              cautions={recipe.recipe.cautions}
+              filter={filter}
             />
           ))}
         </Row>
--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles/Recipe.scss';
 import { Card, Button, ListGroup, ListGroupItem, Accordion, Col } from 'react-bootstrap';
 // Fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
-const Recipie = ({ title, calories, image, ingredients }) => {
+const Recipie = ({ title, calories, image, ingredients, cautions, filter }) => {
+  // filters cautions list and doesnt show that div if there are no cautions
+  const cautionsList = cautions.map(caution => (
+    <li className="caution-list-item">{ caution }</li>
+  ));
+  console.log("filter: ", filter);
 
   return (
-    <Col lg={10} sm={6}>
+    <Col lg={5} sm={10}>
       <Accordion defaultActiveKey="0">
         <Card className="recipe-card">
-          <Card.Img variant="top" src={image} alt={title} />
+          <div className="card-img-container container">
+            <Card.Img variant="top" src={ image } alt={ title } />
+          </div>
           <Card.Header>
-            <h2>{title}</h2>
+            <h2>{ title }</h2>
           </Card.Header>
+          <div className="basic-info-container">
+            <p className="calories">calories: { calories }</p>
+            <ListGroup style={{ display: (cautionsList.length > 0 ? 'inherit' : 'none') }}>
+              <p className="cautions">Cautions:</p>
+              <li style={{ display: (cautionsList.length > 0 ? 'inherit' : 'none') }} className="cautions-list">
+                { cautionsList }
+              </li>
+            </ListGroup>
+            <p className="filter">Filter: { filter }</p>
+          </div>
           <div className="recipie-list-container">
-            <Accordion.Toggle className="expand-button" as={Button} variant="link" eventKey="1">
-              <span>View Recipe</span>
-              <FontAwesomeIcon icon={faCaretDown} color="black" />
+            <Accordion.Toggle className="expand-button" as={ Button } variant="link" eventKey="1">
+              <span>View Ingredients</span>
+              <FontAwesomeIcon icon={ faCaretDown } color="black" />
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="1">
               <Card.Body>
                 <ListGroup className="list-group-flush">
-                  {ingredients.map(ingredient => (<ListGroupItem>{ingredient.text}</ListGroupItem>))}
+                  { ingredients.map(ingredient => (<ListGroupItem>{ingredient.text}</ListGroupItem>)) }
+                </ListGroup>
+              </Card.Body>
+            </Accordion.Collapse>
+          </div>
+          <div className="">
+            <Accordion.Toggle className="expand-button" as={ Button } variant="link" eventKey="1">
+              <span>View Recipe</span>
+              <FontAwesomeIcon icon={ faCaretDown } color="black" />
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>
+                <ListGroup className="list-group-flush">
+                  {
+                    // ingredients.map(ingredient => (<ListGroupItem>{ingredient.text}</ListGroupItem>))
+                  }
                 </ListGroup>
               </Card.Body>
             </Accordion.Collapse>
